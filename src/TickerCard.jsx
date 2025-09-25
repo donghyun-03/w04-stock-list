5e422738ed9e3fe1d76855e66a8a39d5e0760d9a
donghyun <dhyunkang7056@gmail.com>
2025-09-25 17:42:41 +0900

    💄Tailwind

---
+++ src/TickerCard.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+const TickerCard = ({ ticker, name, currentPrice, previousClose }) => {
+  const priceChange = currentPrice - previousClose
+  const isPositive = priceChange >= 0
+
+  return (
+    <div className="bg-white rounded-lg shadow-xl p-6 w-80 transform transition duration-500 hover:scale-105">
+      <div className="flex justify-between items-center mb-4">
+        
+      <h2 className="text-2xl font-bold text-gray-800">{name}</h2>
+        <div className="text-sm font-semibold text-gray-500">{ticker}</div>
+      </div>
+      <div className="border-b border-gray-200 mb-4"></div>
+      
+      <div className={`text-base font-semibold ${isPositive ? 'text-green-700' : 'text-red-700'}`}>
+        ${currentPrice.toFixed(2)}
+      </div>
+
+      <div className={`text-base font-semibold ${isPositive ? 'text-green-700' : 'text-red-700'}`}>
+        {isPositive ? '▲' : '▼'} {priceChange.toFixed(2)}
+      </div>
+      
+      <div className="mt-4 text-sm text-gray-500">
+        전일 종가: ${previousClose.toFixed(2)}
+      </div>
+    </div>
+  )
+}
+
+export default TickerCard
